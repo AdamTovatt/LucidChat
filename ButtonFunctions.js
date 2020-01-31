7cd05de17c3c17c2ba913e61bf36b0edfe6718e2
Adam Tovatt <ato@citygate.se>
2020-01-31 12:59:00 +0100

    fix

--- ButtonFunctions.js
+++ ButtonFunctions.js
@@ -14,3 +14,24 @@ function OpenTab(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+function SubmitName() {
+    var inputBox = document.getElementById("UsernameInput");
+    var value = inputBox.value.trim();
+    var regexp = new RegExp("^(?=.*[a-zA-Z])[a-zA-Z0-9]+$");
+    if (value.length < 2) {
+        alert("Ditt namn måste vara minst två tecken långt");
+        return;
+    }
+    if (value.length > 16) {
+        alert("Ditt namn får inte vara mer än 16 tecken långt");
+        return;
+    }
+    if (regexp.test(value)) {
+        EnterChat(value);
+    }
+    else {
+        alert("Ditt namn får inte innehålla specialtecken");
+        return;
+    }
+}
--- Chat.js
+++ Chat.js
@@ -1,11 +1,32 @@
-var Screen_CreateUser = null;
-var Screen_Chat = null;
+﻿var screen_CreateUser = null;
+var screen_Chat = null;
+
+var api = null;
+var userId = null;
 
 window.onload = function () {
-    Screen_CreateUser = document.getElementById("Screen_CreateUser");
-    Screen_Chat = document.getElementById("Screen_Chat");
+    screen_CreateUser = document.getElementById("Screen_CreateUser");
+    screen_Chat = document.getElementById("Screen_Chat");
 
-    Screen_CreateUser.style.display = "block";
+    screen_CreateUser.style.display = "block";
+
+    api = new Api();
 
     console.log("test");
+}
+
+async function EnterChat(username) {
+    var response = JSON.parse(await api.CreateUser(username));
+    if (response["success"]) {
+        screen_CreateUser.style.display = "none";
+        screen_Chat.style.display = "block";
+        userId = response["id"];
+    }
+    else {
+        if (response.message["code"] == 23505)
+            alert("Det namnet är upptaget just nu");
+        else {
+            alert("Okänt fel, försök igen");
+        }
+    }
 }
