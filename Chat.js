5d17317ba3cd9cf8e021b79d62b8b8e44310d80a
Adam Tovatt <ato@citygate.se>
2020-01-31 14:28:53 +0100

    fix

--- Chat.js
+++ Chat.js
@@ -16,17 +16,20 @@ window.onload = function () {
 }
 
 async function EnterChat(username) {
-    var response = JSON.parse(await api.CreateUser(username));
-    if (response["success"]) {
-        screen_CreateUser.style.display = "none";
-        screen_Chat.style.display = "block";
-        userId = response["id"];
-    }
-    else {
-        if (response.message["code"] == 23505)
+    var response = { success: true, id: 19, message: "ok" };
+    //var response = JSON.parse(await api.CreateUser(username));
+    if (!response["success"]) {
+        if (response.message["code"] == 23505) {
             alert("Det namnet är upptaget just nu");
+            return;
+        }
         else {
             alert("Okänt fel, försök igen");
+            return;
         }
     }
+
+    screen_CreateUser.style.display = "none";
+    screen_Chat.style.display = "block";
+    userId = response["id"];
 }
